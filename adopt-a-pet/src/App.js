867b0288b1c8e1f9fcba146984062b417918e16b
Adam <AGCapdeville@gmail.com>
2022-09-22 17:07:30 -0700

    Update

--- adopt-a-pet/src/App.js
+++ adopt-a-pet/src/App.js
@@ -1,24 +1,33 @@
-import './App.css';
-import React  from 'react';
+import "./App.css";
+import React from "react";
+import logo from "./images/LogoColorTransparentBkg.png";
 
 import {
   Screen,
+  ScreenCard,
+  LargeLogo,
   Modal,
   ModalTitle,
-  ModalSubtitle
+  ModalSubtitle,
+  ModalCenter,
+  ModalFooter,
+  ModalSubmitButton,
 } from "./standards/styledDivs.js";
 
 function App() {
   return (
     <Screen>
-      <Modal>
-        <ModalTitle>
-          Welcome to Adopt-A-Pet
-        </ModalTitle>
-        <ModalSubtitle>
-          Login
-        </ModalSubtitle>
-      </Modal>
+      <ScreenCard>
+        <LargeLogo src={logo} />
+        <Modal>
+          <ModalTitle>Welcome to Adopt-A-Pet</ModalTitle>
+          <ModalSubtitle>Login</ModalSubtitle>
+          <ModalCenter></ModalCenter>
+          <ModalFooter>
+            <ModalSubmitButton>Sign In</ModalSubmitButton>
+          </ModalFooter>
+        </Modal>
+      </ScreenCard>
     </Screen>
   );
 }
--- adopt-a-pet/src/standards/styledDivs.js
+++ adopt-a-pet/src/standards/styledDivs.js
@@ -1,35 +1,98 @@
 import styled from "styled-components";
 
+const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
+  desktopL: `(max-width: ${size.desktop})`,
+};
+
 export const Screen = styled.div`
-    display: flex;
-    width: 100vw;
-    height: 100vh;
-    background: #304c89ff;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background: #ffffff;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ScreenCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 90%;
+  background: #f0f2f5;
+  justify-content: space-around;
+  align-items: center;
+
+  @media ${device.laptop} {
+    flex-direction: column;
+    height: 100%;
+  }
+`;
+
+export const LargeLogo = styled.img`
+  width: 400px;
+  margin-bottom: 200px;
+
+  @media ${device.laptop} {
+    width: 450px;
+    margin-bottom: 0px;
+  }
 `;
 
 export const Modal = styled.div`
-    display: flex;
-    width: 500px;
-    height: 500px;
-    background: #e8e5daff;
-    flex-direction: column;
+  display: flex;
+  width: 500px;
+  height: 500px;
+  background: #ffffff;
+  flex-direction: column;
 `;
 
 export const ModalTitle = styled.div`
-    width: 100%;
-    height: 25%;
-    background: #648de5ff;
+  width: 100%;
+  height: 25%;
+  background: #648de5ff;
 `;
 
 export const ModalSubtitle = styled.div`
-    width: 100%;
-    height: 15%;
-    background: #9eb7e5ff;
+  width: 100%;
+  height: 15%;
+  background: #9eb7e5ff;
+`;
+
+export const ModalSubmitButton = styled.div`
+  width: 20%;
+  height: 10%;
+`;
+
+export const ModalCenter = styled.div`
+  width: 100%;
+  height: 40%;
+`;
+
+export const ModalFooter = styled.div`
+  width: 100%;
+  height: 10%;
 `;
 
 /* CSS HEX */
+// --fbWhite: #ffffff;
+// --fbGrey: #f0f2f5;
 // --sage: #cdc392ff;
 // --alabaster: #e8e5daff;
 // --wild-blue-yonder: #9eb7e5ff;
