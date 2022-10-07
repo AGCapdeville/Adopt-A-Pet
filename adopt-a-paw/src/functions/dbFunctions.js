68adcc615750647ff637e5d51672d06a831c3474
Adam <AGCapdeville@gmail.com>
2022-10-06 17:46:50 -0700

    init backend

---
+++ adopt-a-paw/src/functions/dbFunctions.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require("mongodb");
+
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
+const uri =
+  "mongodb+srv://capdeville:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+
+async function main() {
+  // we'll add code here soon
+}
